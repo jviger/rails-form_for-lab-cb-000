4514a6537d7aad077c1259f2963267692b9f7c51
Jason Viger <jason.viger@gmail.com>
2019-01-29 00:57:42 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -19,5 +19,12 @@ ActiveRecord::Schema.define(version: 1) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+  
+  create_table "school_classes", force: :cascade do |t|
+    t.string   "title"
+    t.integer   "room_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
