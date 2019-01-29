8c49afcb7964353227d76d528f80f7b66dac915c
Jason Viger <jason.viger@gmail.com>
2019-01-29 01:06:33 +0000

    Automatically backed up by Learn

--- db/migrate/20190129010538_school_class.rb
+++ db/migrate/20190129010538_school_class.rb
@@ -1,4 +1,10 @@
 class SchoolClass < ActiveRecord::Migration
   def change
+    create_table :school_classes do |t|
+      t.string :title
+      t.integer :last_name
+
+      t.timestamps null: false
+    end
   end
 end
